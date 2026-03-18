eab799b63fad8998128419bb397b5ff2bd822698
h2t8w94wtj-max <h2t8w94wtj@privaterelay.appleid.com>
2026-03-19 00:01:03 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'siel-pwa-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
